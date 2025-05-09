7212c7c128ba3340f6c0591548596222d75b79a3
Luciano Vernaschi <luciano@cromoteca.com>
2025-05-09 18:22:47 +0200

    start.spring.io

---
+++ src/main/java/com/cromoteca/phrasepal/PhrasePalApplication.java
@@ -0,0 +1,13 @@
+package com.cromoteca.phrasepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrasePalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrasePalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cromoteca/phrasepal/PhrasePalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cromoteca.phrasepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhrasePalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
